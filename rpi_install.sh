b5094fc1c9ccdeb7f258260d7c82a84454ea9413
rdougla3 <rdougla3@asu.edu>
2024-06-23 15:38:02 +0100

    update

--- rpi_install.sh
+++ rpi_install.sh
@@ -1,8 +1,9 @@
 sudo apt update
 sudo apt full-upgrade
-pip install --upgrade pip
 sudo apt-get install portaudio19-dev
 python -m venv jvenv
 deactivate
 source jvenv/bin/activate
-pip3 install -r requirements.txt
+pip install --upgrade pip
+pip3 install -r Jarvis-local/requirements.txt
+pip3 Jarvis-local/rpi_requirements.txt
