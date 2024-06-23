7fc0df2dd568cf2d0aa4303ad594840cbbd8b401
rdougla3 <rdougla3@asu.edu>
2024-06-23 16:23:18 +0100

    update

--- rpi_install.sh
+++ rpi_install.sh
@@ -4,9 +4,10 @@ sudo apt-get install portaudio19-dev
 sudo apt-get install git
 git clone https://github.com/rdougla3/Jarvis-rpi.git
 
-python -m venv Jarvis-rpi/jvenv
+cd Jarvis-rpi
+python -m venv jvenv
 deactivate
 source Jarvis-rpi/jvenv/bin/activate
 pip install --upgrade pip
-pip3 install -r Jarvis-rpi/requirements.txt
-pip3 install -r Jarvis-rpi/rpi_requirements.txt
+pip3 install -r requirements.txt
+pip3 install -r rpi_requirements.txt
