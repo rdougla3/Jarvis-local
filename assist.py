f7b08e891cb3fb287cb9321ee069a0d0ed377d79
Ross Douglas <rdougla3@asu.edu>
2024-06-28 21:21:22 +0100

    update

--- assist.py
+++ assist.py
@@ -5,30 +5,11 @@ from pygame import mixer
 import os
 
 os.environ["OPENAI_API_KEY"] = "your key here"
-assistant_id = "id key here"
-thread_id = "your id here"
 
 #https://platform.openai.com/playground/assistants
 # Initialize the client and mixer
 client = OpenAI(default_headers={"OpenAI-Beta": "assistants=v2"})
 mixer.init()
-
-# Retrieve the assistant and thread
-assistant = client.beta.assistants.retrieve(assistant_id)
-thread = client.beta.threads.retrieve(thread_id)
-
-def ask_question_memory(question):
-    global thread
-    client.beta.threads.messages.create(thread.id, role="user", content=question)
-    run = client.beta.threads.runs.create(thread_id=thread.id, assistant_id=assistant.id)
-    
-    while (run_status := client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)).status != 'completed':
-        if run_status.status == 'failed':
-            return "The run failed."
-        time.sleep(1)
-    
-    messages = client.beta.threads.messages.list(thread_id=thread.id)
-    return messages.data[0].content[0].text.value
 
 def generate_tts(sentence, speech_file_path):
     response = client.audio.speech.create(model="tts-1", voice="echo", input=sentence)
